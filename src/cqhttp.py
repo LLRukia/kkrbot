967a205f4adf5af44019dc853c490cf051d3fb7c
Rukia <rukia.ll.sakura@gmail.com>
2021-11-03 19:43:53 +0800

    polish code

--- src/cqhttp.py
+++ src/cqhttp.py
@@ -1,10 +1,13 @@
 
-import os, argparse
+import os
+import argparse
 from aiocqhttp import CQHttp
 import asyncio
 
 parser = argparse.ArgumentParser(description='cqbot')
-parser.add_argument('-d', '--datapath', type=str, help='static resource path', nargs='*', default=os.path.abspath('../cq/data/'))
+parser.add_argument('-d', '--datapath', type=str, help='static resource path',
+                    nargs='*', default=os.path.abspath('./cq/data/'))
+
 
 def create_bot(server, bot_type='Common'):
     bot = None
@@ -15,34 +18,52 @@ def create_bot(server, bot_type='Common'):
             bot = a(server)
     return bot
 
+
 def main():
     args = parser.parse_args()
-    
+
     import sys
     filedir = os.path.split(os.path.realpath(__file__))[0]
     sys.path.append(os.path.realpath(os.path.join(filedir, 'lib')))
     sys.path.append(filedir)
-    
+
     import logging
     from quart.logging import default_handler
     import const
     const.workpath = os.path.join(filedir, '..')
     const.cachepath = os.path.join(const.workpath, 'cache')
-    const.user_profile_path = os.path.join(const.workpath, 'data', 'user_profile')
+    const.user_profile_path = os.path.join(
+        const.workpath, 'data', 'user_profile')
     const.datapath = args.datapath
-    const.asset_card_path = os.path.join(const.datapath, 'image', 'assets', 'cards')
-    const.asset_card_thumb_path = os.path.join(const.datapath, 'image', 'assets', 'cards', 'thumb')
-    const.asset_event_path = os.path.join(const.datapath, 'image', 'assets', 'events')
-    const.asset_gacha_path = os.path.join(const.datapath, 'image', 'assets', 'gachas')
-    const.asset_resource_path = os.path.join(const.datapath, 'image', 'assets', 'res')
-    
-    if not os.path.exists(const.user_profile_path): os.makedirs(const.user_profile_path)
-    if not os.path.exists(const.asset_card_path): os.makedirs(const.asset_card_path)
-    if not os.path.exists(const.asset_card_thumb_path): os.makedirs(const.asset_card_thumb_path)
-    if not os.path.exists(const.asset_gacha_path): os.makedirs(const.asset_gacha_path)
-    if not os.path.exists(const.asset_resource_path): os.makedirs(const.asset_resource_path)
-    
-    logger_handler = logging.FileHandler(os.path.realpath(os.path.join(const.workpath, 'log/app.log')))
+    const.asset_card_path = os.path.join(
+        const.datapath, 'image', 'assets', 'cards')
+    const.asset_card_thumb_path = os.path.join(
+        const.datapath, 'image', 'assets', 'cards', 'thumb')
+    const.asset_event_path = os.path.join(
+        const.datapath, 'image', 'assets', 'events')
+    const.asset_gacha_path = os.path.join(
+        const.datapath, 'image', 'assets', 'gachas')
+    const.asset_resource_path = os.path.join(
+        const.datapath, 'image', 'assets', 'res')
+
+    if not os.path.exists(const.user_profile_path):
+        os.makedirs(
+            const.user_profile_path)
+    if not os.path.exists(const.asset_card_path):
+        os.makedirs(
+            const.asset_card_path)
+    if not os.path.exists(const.asset_card_thumb_path):
+        os.makedirs(
+            const.asset_card_thumb_path)
+    if not os.path.exists(const.asset_gacha_path):
+        os.makedirs(
+            const.asset_gacha_path)
+    if not os.path.exists(const.asset_resource_path):
+        os.makedirs(
+            const.asset_resource_path)
+
+    logger_handler = logging.FileHandler(os.path.realpath(
+        os.path.join(const.workpath, 'log/app.log')))
     logger_handler.setFormatter(logging.Formatter(
         '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
     ))
@@ -54,11 +75,35 @@ def main():
     cq_server.logger.info(f'datapath = {const.datapath}')
     cq_server.logger.info('begin create bot')
 
-
     def handle_trace(*exc_info):
-        import traceback
-        text = "".join(traceback.format_exception(*exc_info))
-        cq_server.logging.error("Unhandled exception: %s", text)
+        def on_trace_back(type, value, tb):
+            import linecache
+            stacks = []
+            localvars = []
+            try:
+                while tb:
+                    f = tb.tb_frame
+                    c = f.f_code
+                    linecache.checkcache(c.co_filename)
+                    line = linecache.getline(
+                        c.co_filename, f.f_lineno, f.f_globals)
+                    stacks.append('File "%s", in %s\n > %d: %s' % (
+                        c.co_filename, c.co_name, f.f_lineno, line.strip() if line else "N/A"))
+                    f.f_locals.pop("__builtins__", None)
+                    localvars.append('\n'.join(
+                        (("\t%19s : %s" % (n, repr(v)[:128])) for n, v in f.f_locals.iteritems())))
+                    tb = tb.tb_next
+            except Exception as e:
+                stacks.append(str(e))
+
+            stacks.append("%s" % type.__name__)
+            localvars.append(" > %s" % str(value))
+
+            trace = "\n".join(("%s\n%s" % (s, l)
+                              for s, l in zip(stacks, localvars)))
+            return trace
+
+        cq_server.logging.error("Unhandled exception: %s", on_trace_back(*exc_info))
 
     sys.excepthook = handle_trace
 
@@ -67,9 +112,10 @@ def main():
         return
     import socket
     ip_address = socket.gethostbyname(socket.gethostname())
-    cq_server.logger.info('bot created, bind success %s', bot.__class__.__name__)
+    cq_server.logger.info('bot created, bind success %s',
+                          bot.__class__.__name__)
     cq_server.run(host=ip_address, port=8080, debug=False)
-    
-    
+
+
 if __name__ == '__main__':
     main()
---
+++ src/utils/crawler_main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+
+from . import Logger
+from crawler import CardTable, CardCrawler, EventTable, EventCrawler, GachaTable, GachaCrawler
+from crawler.connection import event_gacha
+
+parser = argparse.ArgumentParser(description='crawler for Bestdori assets')
+parser.add_argument('operation', type=str, help='init or update')
+parser.add_argument('-c', '--content', type=str,
+                    help='card, event or gacha', nargs='*')
+parser.add_argument('-d', '--datapath', type=str, help='static resource path',
+                    default=os.path.abspath('./cq/data/image/assets/'))
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    work_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file = os.path.join(work_dir, 'crawler.log')
+    data_dir = os.path.join(os.path.dirname(work_dir), 'data')
+    asset_dir = args.datapath
+    json_dir = os.path.join(data_dir, 'json')
+    db_file = os.path.join(data_dir, 'bestdori.db')
+
+    logger = Logger('crawler', log_file)
+
+    card_crawler = CardCrawler(
+        logger, CardTable(db_file), {
+            'overall_json': json_dir,
+            'json': os.path.join(json_dir, 'cards'),
+            'assets': os.path.join(asset_dir, 'cards'),
+        }, 'https://bestdori.com/api/cards/'
+    )
+
+    event_crawler = EventCrawler(
+        logger, EventTable(db_file), {
+            'overall_json': json_dir,
+            'json': os.path.join(json_dir, 'events'),
+            'assets': os.path.join(asset_dir, 'events'),
+        }, 'https://bestdori.com/api/events/'
+    )
+
+    gacha_crawler = GachaCrawler(
+        logger, GachaTable(db_file), {
+            'overall_json': json_dir,
+            'json': os.path.join(json_dir, 'gachas'),
+            'assets': os.path.join(asset_dir, 'gachas'),
+        }, 'https://bestdori.com/api/gacha/'
+    )
+    if args.operation in ['init', 'update']:
+        content = set(args.content or ['card', 'event', 'gacha']) & set(
+            ['card', 'event', 'gacha'])
+        for c in content:
+            getattr(eval(f'{c}_crawler'), args.operation)()
+        if content >= set(['event', 'gacha']) or 'event-gacha' in args.content:
+            event_gacha(
+                json_dir,
+                db_file,
+                os.path.join(json_dir, 'event_gacha.json'),
+                logger,
+            )
